74f52251f89ecc81a9eaef4552709d13f6d5bd60
sampathotm <sampath.otm@gmail.com>
2018-07-08 01:27:54 +0530

    testing docker

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,7 +1,14 @@
 cd /var/lib/jenkins/workspace/docker/
 
+sudo docker rmi -f mycentos:httpdv1.0 .
+
 sudo docker build -t mycentos:httpdv1.0 .
 
 sudo mkdir /data
 
-sudo docker run -td -p 80:80 -v /data:/var/www/html --name=web mycentos:httpdv1.i0
+sudo docker stop web
+
+sudo docker rm web
+
+####
+sudo docker run -td -p 80:80 -v /data:/var/www/html --name=web mycentos:httpdv1.0
